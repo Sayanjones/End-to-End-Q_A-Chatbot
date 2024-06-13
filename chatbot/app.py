752b0c6215712464b1643d63439c285edde33294
Sayan Mandal <155817507+Sayanjones@users.noreply.github.com>
2024-06-13 16:32:48 +0530

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,10 +1,9 @@
-from langchain_openai import ChatOpenAI                   ## ChatOpenAI will be helpful in interacting with LLM models
-from langchain_core.prompts import ChatPromptTemplate     ## if we want to write a custom promt then ChatPromptTemplate will provide tempalte
-from langchain_core.output_parsers import StrOutputParser ## whatever output or response would come from llm model that this library will show us
-
+from langchain_openai import ChatOpenAI                 
+from langchain_core.prompts import ChatPromptTemplate    
+from langchain_core.output_parsers import StrOutputParser 
 import streamlit as st
-import os                                                 ## will call all environment variables
-from dotenv import load_dotenv                            ## will load all environment variables
+import os                                                
+from dotenv import load_dotenv                           
 
 load_dotenv()
 
@@ -12,7 +11,7 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 
 ## Langmith tracking  (monitoring, debugging, tracing)
-os.environ["LANGCHAIN_TRACING_V2"]="true"                 ## we need to initialize another parameter(set true for tracing the project name that have created "Tutorial")
+os.environ["LANGCHAIN_TRACING_V2"]="true"                 
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
 ## creating chatbot
@@ -29,12 +28,12 @@ prompt=ChatPromptTemplate.from_messages(
 st.title('Langchain Demo With OPENAI API')
 input_text=st.text_input("Search the topic u want")
 
-## openAI LLmM call
+## openAI LLm call
 llm=ChatOpenAI(model="gpt-3.5-turbo")
 output_parser=StrOutputParser()
 
 ## chain
-chain=prompt|llm|output_parser                                           ## making chain so that we can get multiple option like 'or' ..
+chain=prompt|llm|output_parser                                           ## making chain so that we can get multiple options like 'or' ..
 
 if input_text:
     st.write(chain.invoke({'question':input_text}))               
