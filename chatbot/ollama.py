55909b9df565fdd10825277bcce5c1915e0a1f9f
Sayan Mandal <155817507+Sayanjones@users.noreply.github.com>
2024-06-13 16:33:43 +0530

    Update ollama.py

--- chatbot/ollama.py
+++ chatbot/ollama.py
@@ -1,15 +1,15 @@
-from langchain_openai import ChatOpenAI                   ## ChatOpenAI will be helpful in interacting with LLM models
-from langchain_core.prompts import ChatPromptTemplate     ## if we want to write a custom promt then ChatPromptTemplate will provide tempalte
-from langchain_core.output_parsers import StrOutputParser ## whatever output or response would come from llm model that this library will show us
+from langchain_openai import ChatOpenAI                  
+from langchain_core.prompts import ChatPromptTemplate    
+from langchain_core.output_parsers import StrOutputParser  
 from langchain_community.llms import Ollama
 import streamlit as st
-import os                                                 ## will call all environment variables
-from dotenv import load_dotenv                            ## will load all environment variables
+import os                                                
+from dotenv import load_dotenv                          
 
 load_dotenv()
 
 ## Langmith tracking  (monitoring, debugging, tracing)
-os.environ["LANGCHAIN_TRACING_V2"]="true"                 ## we need to initialize another parameter(set true for tracing the project name that have created "Tutorial")
+os.environ["LANGCHAIN_TRACING_V2"]="true"                 
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
 ## creating chatbot
@@ -31,7 +31,7 @@ llm=Ollama(model="llama2")
 output_parser=StrOutputParser()
 
 ## chain
-chain=prompt|llm|output_parser                                           ## making chain so that we can get multiple option like 'or' ..
+chain=prompt|llm|output_parser                                           ## making chain so that we can get multiple options like 'or' ..
 
 if input_text:
     st.write(chain.invoke({'question':input_text}))               
